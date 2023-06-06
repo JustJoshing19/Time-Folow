828b2c47d4565c18482d2ac5ce85158168a0481a
Joshua Roux <rxxjos004@myuct.ac.za>
2023-06-06 10:17:22 +0200

    Created model for post made by users

--- social_platform/TimeFollow/models.py
+++ social_platform/TimeFollow/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here.
 class Post(models.Model):
-    pass
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    postContent = models.CharField(max_length=200, null=False)
+    timeStamp = models.DateTimeField(null=False)
